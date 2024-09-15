9ebde9feda731c7e2c7444d88a3494503a5e92f9
reyad-hasan <akmkamrulhasanreyad@gmail.com>
2024-09-15 13:14:16 +0600

    solve some task

---
+++ practies.js
@@ -0,0 +1,38 @@
+// const school = "Rajuk High School";
+// console.log(school);
+// console.log(school.toUpperCase());
+// console.log(school.toLowerCase());
+
+// const school2 = "rajuk high School";
+
+// if (school.toUpperCase() === school2.toUpperCase()) {
+//   console.log("Same things");
+// } else {
+//   console.log("Not Same things");
+// }
+
+// const slice2 = school.slice(2, 5);
+// console.log(slice2);
+
+// const split2 = school.split(" ");
+// console.log(split2);
+
+// const realFriends = "Rajuk,High,School";
+// console.log(realFriends.split("h"));
+// const realFriends = ["Ridwan", "Rone", "Reyad"];
+// console.log(realFriends.join("+"));
+
+const bestFriend = "Ridwan Tanvir";
+const bestFriend2 = "Rone Rahman";
+
+// console.log(bestFriend + "," + bestFriend2);
+
+// const bestFriend3 = bestFriend
+//   .concat(" + ")
+//   .concat(bestFriend2)
+//   .concat(" + ")
+//   .concat(bestFriend);
+// console.log(bestFriend3);
+
+const have = bestFriend.includes("R");
+console.log(have);
