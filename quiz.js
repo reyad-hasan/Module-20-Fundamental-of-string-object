f4e2c8e22183e98ff0ea8c9aac9144ed0c21c21f
reyad-hasan <akmkamrulhasanreyad@gmail.com>
2024-09-15 12:45:58 +0600

    added task file

---
+++ quiz.js
@@ -0,0 +1,31 @@
+// const x = ""
+// if (!x.length) {
+//     console.log("Hey")
+// } else {
+//     console.log("Hi")
+// }
+
+
+// const str = "Hello";
+// str[0] = "h";
+// console.log(str);
+
+
+// let str = "Mississippi";
+// console.log(str.indexOf("i", 3));
+
+
+// const myObject = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+//    };
+   
+//    for (let prop in myObject) {
+//     console.log(myObject[prop]);
+//    }
+
+var person = {};
+person.name = "John";
+person.age = 30;
+console.log(person)
